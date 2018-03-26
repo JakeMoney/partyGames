664a9b4350ef0c3cd429b134f99f6707e7d1183b
Michael Pratt <mkpratt6@gmail.com>
2018-03-26 10:34:47 -0600

    initial commit, untested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { Api } from './api';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { JoinRoomComponent } from './join-room/join-room.component';
+import { WaitingRoomComponent } from './waiting-room/waiting-room.component';
+import { GameSelectComponent } from './game-select/game-select.component';
 
+import './rxjs-operators';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateRoomComponent,
+    JoinRoomComponent,
+    WaitingRoomComponent,
+    GameSelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [Api],
+  bootstrap: [AppComponent],
+  entryComponents: []
 })
+
 export class AppModule { }
---
+++ src/app/models/room.ts
@@ -0,0 +1,6 @@
+export interface IRoom {
+  id: string;
+  hostId: string;
+  gameType: string;
+  passCode: string;
+}

ba7efed444c93bc1f12ce207482581647ee7d12f
michaelpratt28 <michaelpratt28@gmail.com>
2018-03-27 12:52:36 -0600

    updates to the create room page

--- src/app/create-room/create-room.component.ts
+++ src/app/create-room/create-room.component.ts
@@ -9,7 +9,6 @@ export class CreateRoomComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
--- src/app/join-room/join-room.component.ts
+++ src/app/join-room/join-room.component.ts
@@ -9,7 +9,6 @@ export class JoinRoomComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
